913cffbd115523c0a2a414497085a1a43a99f5c7
Marcello Krahforst <mk@example.com>
2026-07-01 06:08:35 +0200

    Add additional build caches

--- modules/system/nix/nix.nix
+++ modules/system/nix/nix.nix
@@ -17,11 +17,17 @@
         "cgroups" # allow nix to execute builds inside cgroups
       ];
       substituters = [
-        "https://helix.cachix.org" # a cache for helix
+        "https://helix.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nixpkgs-wayland.cachix.org"
       ];
       warn-dirty = false;
       trusted-public-keys = [
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSeBo="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nixpkgs-wayland.cachix.org-1:NnKnit196/ozcWSySqNdObLMRqtW+xJNEHlBMRFmFfo="
       ];
     };
   };
